cb4fd611bd805f95cb81892610838817e4bd255c
Mahdi Hosseini <mahdidvb72@gmail.com>
2020-06-10 21:02:00 +0430

    fix skin change

--- HandyWinGet-GUI/App.xaml.cs
+++ HandyWinGet-GUI/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using HandyControl.Controls;
 using HandyControl.Data;
+using HandyControl.Themes;
 using HandyControl.Tools;
 using HandyWinget_GUI.Assets;
 using HandyWinget_GUI.Assets.Languages;
@@ -80,6 +81,7 @@ namespace HandyWinget_GUI
         }
         internal void UpdateSkin(SkinType skin)
         {
+            SharedResourceDictionary.SharedDictionaries.Clear();
             Resources.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
             Resources.MergedDictionaries.Add(new ResourceDictionary
             {
